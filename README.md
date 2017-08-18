c048843f01e367a792e4eed9429c2d256d6c2ed4
harry.yue <harry.yue@delphi.com>
2017-08-18 10:32:12 +0800

    [Shell] submit the library check and patch apply script
    Signed-off-by: harry.yue <harry.yue@delphi.com>

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 #Scripe
 
 #用于放置各种脚本
-#1.Makefile	=>	linux驱动外部模块编译脚本
-#2.build_intel.sh	=>	intel baytral Android编译脚本
-#3.flashimage_intel.sh	=>	intel baytral Android烧写image脚本
-#4.compile.sh	=>	调用脚本
-#5.push_ko.py	=>	android用于push驱动模块的脚本
-#6.patchcopy_1st.py	=>	用于将git format patch分类存储(初版)
-#7.patchcopy_2st.py	=>	用于将git format patch分类存储(修正版)
-#8.patchcopy_3st.py	=>	用于将git format patch分类存储(修正版更新)
+#001.Makefile				=>		linux驱动外部模块编译脚本
+#002.build_intel.sh			=>		intel baytral Android编译脚本
+#003.flashimage_intel.sh	=>		intel baytral Android烧写image脚本
+#004.compile.sh				=>		调用脚本
+#005.push_ko.py				=>		android用于push驱动模块的脚本
+#006.patchcopy_1st.py		=>		用于将git format patch分类存储(初版)
+#007.patchcopy_2st.py		=>		用于将git format patch分类存储(修正版)
+#008.patchcopy_3st.py		=>		用于将git format patch分类存储(修正版更新)
+#009.lib_use_check.sh 		=>  	用于找出rootfs中没有被使用的library
+#010.apply_patch.sh 		=> 		用于将一个分支的patch打到另外一个分支上	
---
+++ apply_patch.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PRE="/home/user/M2Hypervisor/"
+PATCH_STOR="/home/user/pro/patch/\[3\]Cyber_Sercurity_Update/"
+LINUX="$PRE/dom0/kernel_dom0/"
+ANDROID="$PRE/domU/android/kernel/android-4.4/"
+LINUX_BRANCH="cyber_security_local"
+ANDROID_BRANCH="cyber_security"
+
+echo "###############  Apply patch to Android kernel  ###############"
+if [ -z $1 ]
+then
+	echo "No set commit ID..."
+	echo "Usage:bash $0 commit_id"
+	exit 1
+fi
+
+mkdir -p $PRE/tmp_patch
+
+echo "[1/3]Generate patch in linux kernel's $LINUX_BRANCH branch..."
+cd $LINUX
+#Generate the patch
+git checkout $LINUX_BRANCH
+git format-patch $1
+mv *.patch $PRE/tmp_patch
+echo "[1/3]done."
+
+echo "[2/3]Apply the patch to Android kernel's $ANDROID_BRANCH branch..."
+cd $ANDROID
+git checkout $ANDROID_BRANCH
+git checkout -- .
+
+ls $PRE/tmp_patch | while read line
+do
+	echo $line
+	git am $PRE/tmp_patch/$line
+done
+echo "[2/3]done."
+
+echo "[3/3]Compiling the Android kernel..."
+cd $PRE
+make target/domU_android/install V=99
+cp $PRE/tmp_patch/*.patch $PATCH_STOR
+echo "[3/3]done."
+echo "###############  Finish  ###############"
