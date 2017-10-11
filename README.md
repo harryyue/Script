c0baab6e5041deb77a323c042c6c1fd2b330cca7
harry.yue <harry.yue@delphi.com>
2017-10-11 15:48:33 +0800

    Shell:Create a script to build release image
    Using build_full_ici2_image.sh script,we will get the release image.
    
    Added:
    	build_full_ici2_image.sh
    
    Signed-off-by: harry.yue <harry.yue@delphi.com>

--- README.md
+++ README.md
@@ -11,3 +11,4 @@
 #008.apply_patch.sh 								=> 		用于将一个分支的patch打到另外一个分支上	
 #009.lib_classification_release.sh  				=>      用于找出rootfs中没有被使用的library，并且分类(初选)
 #010.slim_rootfs_release.sh 						=> 		读取指定文件内容，然后依次删除rootfs中的对应文件，实现rootfs的slim
+#011.build_full_ici2_image.sh 						=> 		用于生成ICI2.0的Relese image.
---
+++ build_full_ici2_image.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#Variable
+CUR_PATH=`pwd`
+MIRROR_PATH="/mnt/ngi2-integration-4.4/"
+BSP_PATH="/home/user/M2Hypervisor/"
+AP_PATH="/home/user/pro/cc0818aa_AP/"
+ODC_PATH="/home/user/pro/ici2/ngi20_linux/linux-rootfs/"
+ANDROID_PATH="/home/user/pro/Android_AP/"
+
+RED="\033[31;1m"
+GREEN="\033[32;1m"
+RESET="\033[0m"
+
+set -e
+
+if [ $# -lt 2 -o $# -gt 3 ]
+then
+	echo -e $RED"[ERROR]The numbers of variable should be 2 or 3."$RESET 
+	echo "Usage:"
+	echo "       bash $0 (hardware) (bench) [choose]  "
+	echo "Example:"
+	echo "         bash $0 dv1 k226 all  "
+	exit 1
+fi
+
+echo  -e $GREEN"Start..."$RESET
+echo "[1/6]Prepare the mirror store."
+test -d $MIRROR_PATH && cd $MIRROR_PATH || (echo -e $RED "[ERROR]Couldn't access $MIRROR_PATH ." $RESET ;exit 1)
+./cleanall && ./pre-integration
+echo "[1/6]done."
+
+echo "[2/6]Merge the AP component to mirror store."
+test -d $AP_PATH && cd $AP_PATH || (echo -e $RED "[ERROR]Couldn't access $AP_PATH ." $RESET ;exit 1)
+./build $2
+test -d tools/scripts && cd tools/scripts || (echo -e $RED "[ERROR]Couldn't access $AP_PATH/tools/scripts ." $RESET ; exit 1)
+./merge.sh $2
+echo "[2/6]done."
+
+echo "[3/6]Merge the ODC component to mirror store."
+test -d $ODC_PATH && cd $ODC_PATH || (echo -e $RED "[ERROR]Couldn't access $ODC_PATH ." $RESET ;exit 1)
+sudo cp -r usr/app usr/odc usr/rpc $MIRROR_PATH/merge/usr
+sudo cp -r var/odc var/sdk $MIRROR_PATH/merge/var
+echo "[3/6]done."
+
+echo "[4/6]Create the release image."
+cd $MIRROR_PATH
+./pos-integration
+echo "[4/6]done."
+
+echo "[5/6]Update the BSP component to mirror store."
+if [ $# = 3 ]
+then
+	test -d $BSP_PATH && cd $BSP_PATH || (echo -e $RED "[ERROR]Couldn't access $BSP_PATH ." $RESET ;exit 1)
+	if [ $3 = "all" ]
+	then
+		./ReleaseBsp.sh $1
+	elif [ $3 = "kernel" ]
+	then
+		./BuildKernel.sh
+	fi
+	./BspMerge.sh
+else
+	echo "Not Update the BSP component."
+fi
+echo "[5/6]done."
+
+echo "[6/6]Merge the Android component to mirror store."
+test -d $ANDROID_PATH && cd $ANDROID_PATH || (echo -e $RED "[ERROR]Couldn't access $ANDROID_PATH ." $RESET ; exit 1)
+if [ $# = 3 ]
+then
+	test $3 = "all" && sudo cp -r *.img $MIRROR_PATH/release || echo "Not Update the Android component."
+else
+	echo "Not Update the Android component."
+fi
+echo "[6/6]done."
+
+echo -e $GREEN"Finish..."$RESET
