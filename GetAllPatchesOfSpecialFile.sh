aedf37b00fd150a8f5fcac4eaada96fce56ad628
harry.yue <harry.yue@delphi.com>
2018-08-09 13:12:46 +0800

    [SHELL]Update the GetAllPatchesOfSpecialFile.sh
    Add feature:
    1.support get git patch according to author
    
    Signed-off-by: harry.yue <harry.yue@delphi.com>

--- GetAllPatchesOfSpecialFile.sh
+++ GetAllPatchesOfSpecialFile.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
-if [ $# != 2 ];then
-	echo "[ERROR] Please input correct params."
+while getopts "o:a:f:" arg
+do
+	case $arg in
+		o)
+			OUTPUT_DIR="$OPTARG"
+			;;
+		a)
+			AUTHOR="$OPTARG"
+			;;
+		f)
+			CHECK_FILE="$OPTARG"
+			;;
+		?)
+			echo "Usage:"
+			echo "$0 -o <output_path> [-a <author>] [-f <special_file_name>]"
+			exit 1
+	esac
+done
+
+if [ "Y"$OUTPUT_DIR = "Y" ];then
+	echo "[ERROR] Please input output_path"
 	echo "Usage:"
-	echo "$0 <special_file_name> <patch_store_path>"
+	echo "$0 -o <output_path> [-a <author>] [-f <special_file_name>]"
 	exit 1
-fi
-
-if [ ! -f $1 ];then
-	echo "[ERROR] $1 doesn't exist."
-	exit 1
-fi
-
-if [ ! -d $2 ];then
+elif [ ! -d $OUTPUT_DIR ];then
 	echo "Create the patch store path."
-	mkdir -p $2
+	mkdir -p $OUTPUT_DIR
+fi
+
+
+if [ "Y"$CHECK_FILE != "Y" ];then
+	if [ ! -f $CHECK_FILE ];then
+		echo "[ERROR] $CHECK_FILE doesn't exist."
+		exit 1
+	fi
 fi
 
 echo "[1/3]Get all of commit id for special file."
-git log --oneline $1 > $2/commit_id.txt
+git log --oneline $CHECK_FILE --author=$AUTHOR > $OUTPUT_DIR/commit_id.txt
 
-echo "[2/3]Get all of patch and move it to $2 ."
+echo "[2/3]Get all of patch and move it to $OUTPUT_DIR ."
 #calculatint the commit_id.txt line number
-TOTAL_LINES=`sed -n "$=" $2/commit_id.txt`
+TOTAL_LINES=`sed -n "$=" $OUTPUT_DIR/commit_id.txt`
 CUR_LINE=$TOTAL_LINES
 while read line
 do
 	echo "$CUR_LINE"
 	COMMIT_ID=`echo $line |cut -f 1 -d " "`
-	git format-patch $COMMIT_ID -1 -o $2 --start-number $CUR_LINE --subject-prefix "PATCH $CUR_LINE/$TOTAL_LINES"
+	git format-patch $COMMIT_ID -1 -o $OUTPUT_DIR --start-number $CUR_LINE --subject-prefix "PATCH $CUR_LINE/$TOTAL_LINES"
 	((CUR_LINE--))
 done < $2/commit_id.txt
 
