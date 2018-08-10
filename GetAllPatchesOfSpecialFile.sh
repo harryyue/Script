79c265d975b788b6e94ab4e007f30190d1853ac4
harry.yue <harry.yue@delphi.com>
2018-08-10 10:28:11 +0800

    [SHELL] Fix error of GetAllPatchesOfSpecialFile.sh
    add "-r" option for supporting rrmove old patch in store path
    
    modified:
    	1.GetAllPatchesOfSpecialFile.sh
    
    Signed-off-by: harry.yue <harry.yue@delphi.com>

--- GetAllPatchesOfSpecialFile.sh
+++ GetAllPatchesOfSpecialFile.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#
+# File Name: 	GetAllPatchesOfSpecialFile.sh
+# Author: 		Harry.Yue
+# Create: 		2018.07.25
+# Modify: 		2018.08.10
+#
 
-while getopts "o:a:f:" arg
+while getopts "o:a:f:r" arg
 do
 	case $arg in
 		o)
@@ -12,9 +18,12 @@ do
 		f)
 			CHECK_FILE="$OPTARG"
 			;;
+		r)
+			CLEAN="yes"
+			;;
 		?)
 			echo "Usage:"
-			echo "$0 -o <output_path> [-a <author>] [-f <special_file_name>]"
+			echo "$0 -o <output_path> [-a <author>] [-f <special_file_name>] [-r]"
 			exit 1
 	esac
 done
@@ -27,18 +36,21 @@ if [ "Y"$OUTPUT_DIR = "Y" ];then
 elif [ ! -d $OUTPUT_DIR ];then
 	echo "Create the patch store path."
 	mkdir -p $OUTPUT_DIR
+elif [ "T"$CLEAN = "Tyes" ];then
+	echo "Remove old files."
+	rm -rf $OUTPUT_DIR
+	mkdir -p $OUTPUT_DIR
 fi
 
-
 if [ "Y"$CHECK_FILE != "Y" ];then
-	if [ ! -f $CHECK_FILE ];then
+	if [[ ! -f $CHECK_FILE && ! -d $CHECK_FILE ]];then
 		echo "[ERROR] $CHECK_FILE doesn't exist."
 		exit 1
 	fi
 fi
 
 echo "[1/3]Get all of commit id for special file."
-git log --oneline $CHECK_FILE --author=$AUTHOR > $OUTPUT_DIR/commit_id.txt
+git log --oneline --author=$AUTHOR $CHECK_FILE > $OUTPUT_DIR/commit_id.txt
 
 echo "[2/3]Get all of patch and move it to $OUTPUT_DIR ."
 #calculatint the commit_id.txt line number
