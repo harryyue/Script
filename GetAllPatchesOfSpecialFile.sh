15ca2f0fe2a8e5de0f268186618d0d6b126203b0
harryyue <834500998@qq.com>
2019-03-25 17:30:33 +0800

    [Shell] Update GetAllPatchesOfSpecialFile.sh
        Add two option:-s and -e.
        -s used to specify the start commit id
        -e used to specify the end commit id
        By -s and -e,we could get the patches which between range -s and -e.

--- GetAllPatchesOfSpecialFile.sh
+++ GetAllPatchesOfSpecialFile.sh
@@ -3,10 +3,16 @@
 # File Name: 	GetAllPatchesOfSpecialFile.sh
 # Author: 		Harry.Yue
 # Create: 		2018.07.25
-# Modify: 		2018.08.10
+# Modify: 		2019.03.25
 #
 
-while getopts "o:a:f:r" arg
+Usage() {
+	echo "Usage:"
+	echo "$1 -o <output_path> [-a <author>] [-n <special_file_name>] [-s <start commit id>] [-e <end commit id>] [-r]"
+	exit 1
+}
+
+while getopts "o:a:n:s:e:r" arg
 do
 	case $arg in
 		o)
@@ -16,23 +22,31 @@ do
 			AUTHOR="$OPTARG"
 			;;
 		f)
+			START_DATE="$OPTARG"
+			;;
+		n)
 			CHECK_FILE="$OPTARG"
+			;;
+		t)
+			END_DATE="$OPTARG"
+			;;
+		s)
+			START_ID="$OPTARG"
+			;;
+		e)
+			END_ID="$OPTARG"
 			;;
 		r)
 			CLEAN="yes"
 			;;
 		?)
-			echo "Usage:"
-			echo "$0 -o <output_path> [-a <author>] [-f <special_file_name>] [-r]"
-			exit 1
+			Usage $0
 	esac
 done
 
 if [ "Y"$OUTPUT_DIR = "Y" ];then
 	echo "[ERROR] Please input output_path"
-	echo "Usage:"
-	echo "$0 -o <output_path> [-a <author>] [-f <special_file_name>]"
-	exit 1
+	Usage $0
 elif [ ! -d $OUTPUT_DIR ];then
 	echo "Create the patch store path."
 	mkdir -p $OUTPUT_DIR
@@ -49,8 +63,28 @@ if [ "Y"$CHECK_FILE != "Y" ];then
 	fi
 fi
 
+if [ "Y"$START_ID != "Y" ];then
+	START_ID="$START_ID.."
+fi
+
+#if [ "Y"$START_DATE = "Y" ];then
+#	START_DATE=19700101
+#	echo "[INFO] use $START_DATE as the begin date"
+#fi
+
+#if [ "Y"$END_DATE = "Y" ];then
+#	END_DATE=`data +%Y%m%d`
+#	((END_DATE ++))
+#	echo "[INFO] use $END_DATE as the end date"
+#fi
+
+#if [ $END_DATE -lt $START_DATE ];then
+#	echo "[ERROR] Invalid value."
+#	Usage $0
+#fi
+
 echo "[1/3]Get all of commit id for special file."
-git log --oneline --author=$AUTHOR $CHECK_FILE > $OUTPUT_DIR/commit_id.txt
+git log --oneline --author=$AUTHOR $START_ID$END_ID $CHECK_FILE > $OUTPUT_DIR/commit_id.txt
 
 echo "[2/3]Get all of patch and move it to $OUTPUT_DIR ."
 #calculatint the commit_id.txt line number
